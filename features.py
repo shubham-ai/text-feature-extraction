fdec83118d70260f01b171ad73321279c1393be6
Vaibhav <vaibhav@1touch-dev.com>
2020-09-04 18:30:49 +0530

    Pattern Matching

--- features.py
+++ features.py
@@ -1,28 +1,31 @@
 import spacy
+from spacy.matcher import Matcher
 
 nlp = spacy.load('en_core_web_lg')
+matcher = Matcher(nlp.vocab)
 
 with open('news.txt', 'r', encoding='utf-8') as f:
     data = f.read()
 
 features = data.split('. ')
 
-text = nlp(features[0])
+text = nlp(features[2])
 
 print(text)
 names = []
 events = []
+references = []
 dates = []
 money = []
-
+proper_nouns = []
 
 for ent in text.ents:
-    print(ent.text, ent.label_)
     if ent.label_ == "MONEY":
         money.append(ent.text)
 
     if ent.label_ == "DATE":
         dates.append(ent.text)
+    
 
 iterator = 0
 
@@ -35,6 +38,7 @@ for token in text:
         continue
 
     if token.pos_ == "PROPN":
+        proper_nouns.append(token.text)
         b = [i for i in dates if i.find(token.text) > -1]
         c = [i for i in money if i.find(token.text) > -1]
         if (len(b) == 0 and len(dates) != 0 or (len(c) == 0 and len(money) != 0)):
@@ -67,7 +71,31 @@ for token in text:
             print("End of text")
 
 
+match = [{"POS":"PROPN"},
+        {"POS":"ADP", "OP":"?"},
+        {"POS":"PROPN", "OP":"?"},
+        {"POS":"VERB", "OP":"?"}]
+
+action = [{"POS":"VERB"},
+            {"POS":"ADJ", "OP":"?"},
+            {"POS":"ADP", "OP":"?"},
+            {"POS":"DET", "OP":"?"},
+            {"POS":"NOUN", "OP":"?"}]
+
+matcher.add("Context", None, match)
+matcher.add("action", None, action)
+matches = matcher(text)
+
+remove_duplicates = [text[start:end] for _, start, end in matches]
+span = spacy.util.filter_spans(remove_duplicates)
+
+
+for i in span:
+    references.append(i)
+
+
 print(names)
 print(events)
+print(references)
 print(dates)
 print(money)
